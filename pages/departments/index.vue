9fc02561e0b5588ab196ca8be5c328a464fa1e60
Alberto Escobar <128758041+FernandoHernandezCruz@users.noreply.github.com>
2024-07-22 15:03:27 -0600

    feat(RHATM0103M003031): Sync de departamentos en BO

--- pages/departments/index.vue
+++ pages/departments/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="department-page">
     <Toast />
+
     <Head>
       <Title>
         Departments
@@ -13,7 +14,18 @@
             <label for="search">
               Search
             </label>
-            <InputText v-model="search" aria-describedby="search" @keypress="handlerSearchDepartment" @keyup.delete="handlerSearchDepartment" />
+            <InputText v-model="search" aria-describedby="search" @keypress="handlerSearchDepartment"
+              @keyup.delete="handlerSearchDepartment" />
+          </div>
+          <div class="input-box">
+            <br />
+            <Button class="btn-add mr-2" @click="syncDepartments">
+              <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" class="icon">
+                <path
+                  d="M41.115 20.33C40.213 9.822 31.79 1.478 21.248.681c6.77 3.647 11.373 10.798 11.373 19.026 0 .211-.026.414-.032.623h-7.793l11.563 13.779L47.923 20.33h-6.808zM15.155 28.293c0-.234.026-.461.034-.692h8.015L11.642 13.822.077 27.601h6.579c.872 10.54 9.307 18.921 19.87 19.719-6.769-3.649-11.371-10.799-11.371-19.027z"
+                  fill="#ffffff"></path>
+              </svg>
+            </Button>
           </div>
         </div>
         <div>
@@ -21,13 +33,12 @@
             Departments
           </h2>
           <div class="department-card-wrapper">
-            <div v-for="(department, index) in filteredDepartments" :key="`department-${department.departmentId}-${index}`">
-              <DepartmentInfoCard
-                :department="department"
-              />
+            <div v-for="(department, index) in filteredDepartments"
+              :key="`department-${department.departmentId}-${index}`">
+              <DepartmentInfoCard :department="department" />
             </div>
           </div>
-          
+
           <div></div>
           <!-- <Paginator 
               class="paginator"
@@ -50,8 +61,8 @@
 </template>
 
 <script>
-  import Script from './script.ts'
-  export default Script
+import Script from './script.ts'
+export default Script
 </script>
 
 <style lang="scss" scoped>
@@ -67,13 +78,14 @@
   max-width: 50rem !important;
 
   @media screen and (max-width: $sm) {
-     width: 100% !important;
-   }
+    width: 100% !important;
+  }
 }
 </style>
 
 <style lang="scss">
 @import '/resources/styles/variables.scss';
+
 :deep(.graph-label) {
   color: red;
 }
