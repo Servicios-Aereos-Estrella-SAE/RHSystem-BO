f4af74585e164eaaffa3e2d2281de046782efc49
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2024-08-13 17:49:24 -0600

    fix(SAEPB0111 Pilotos): SAEPB0111M001002 Catálogo de pilotos
    se creo el crud de pilotos con sus validaciones

---
+++ components/pilotInfoCard/script.ts
@@ -0,0 +1,37 @@
+import { DateTime } from 'luxon'
+import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
+import type { PilotInterface } from '~/resources/scripts/interfaces/PilotInterface'
+
+export default defineComponent({
+  name: 'PilotInfoCard',
+  props: {
+    pilot: { type: Object as PropType<PilotInterface>, required: true },
+    clickOnEdit: { type: Function, default: null },
+    clickOnDelete: { type: Function, default: null }
+  },
+  data: () => ({
+  }),
+  computed: {
+    hireDate() {
+      if (this.pilot && this.pilot.pilotHireDate) {
+        return DateTime.fromISO(this.pilot.pilotHireDate.toString()).toFormat('DD')
+      }
+      return ''
+    }
+  },
+  async mounted() {
+  },
+  methods: {
+    handlerClickOnEdit () {
+      if (this.clickOnEdit) {
+        this.clickOnEdit()
+      }
+    },
+    handlerClickOnDelete () {
+      if (this.clickOnDelete) {
+        this.clickOnDelete()
+      }
+    },
+  }
+})
--- resources/scripts/services/PersonService.ts
+++ resources/scripts/services/PersonService.ts
@@ -1,3 +1,5 @@
+import type { PeopleInterface } from "../interfaces/PeopleInterface"
+
 export default class PersonService {
   protected API_PATH: string
 
@@ -24,5 +26,92 @@ export default class PersonService {
         }
     } catch (error) {
     }
+  }
+
+  async store(person: PeopleInterface) {
+    let responseRequest: any = null
+    try {
+      await $fetch(`${this.API_PATH}/persons`, {
+        method: 'POST',
+        query: { ...person },
+        onResponse ({ response }) { responseRequest = response },
+        onRequestError ({ response }) { responseRequest = response }
+      })
+    } catch (error) {
+    }
+    return responseRequest
+  }
+
+  async update(person: PeopleInterface) {
+    let responseRequest: any = null
+    try {
+      await $fetch(`${this.API_PATH}/persons/${person.personId}`, {
+        method: 'PUT',
+        query: { ...person },
+        onResponse ({ response }) { responseRequest = response },
+        onRequestError ({ response }) { responseRequest = response }
+      })
+    } catch (error) {
+    }
+    return responseRequest
+  }
+
+  isValidRFC(rfc: string, aceptarGenerico: Boolean = true) {
+    if (!rfc) { return false }
+  
+    const re = /^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/
+    const validado = rfc.match(re)
+    if (!validado) { return false }
+  
+    // Separar el dígito verificador del resto del RFC
+    const digitoVerificador = `${validado.pop()}`
+    const rfcSinDigito = validado.slice(1).join('')
+    const len = rfcSinDigito.length
+  
+    // Obtener el digito esperado
+    const diccionario = '0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ Ñ'
+    const indice = len + 1
+    let suma,
+      digitoEsperado
+  
+    if (len === 12) { suma = 0 } else { suma = 481 } // Ajuste para persona moral
+  
+    for (let i = 0; i < len; i++) { suma += diccionario.indexOf(rfcSinDigito.charAt(i)) * (indice - i) }
+    digitoEsperado = 11 - suma % 11
+    if (digitoEsperado === 11) { digitoEsperado = 0 } else if (digitoEsperado === 10) { digitoEsperado = 'A' }
+    digitoEsperado = `${digitoEsperado}`
+  
+    // El dígito verificador coincide con el esperado?
+    // o es un RFC Genérico (ventas a público general)?
+    if ((digitoVerificador !== digitoEsperado) && (!aceptarGenerico || rfcSinDigito + digitoVerificador !== 'XAXX010101000')) {
+      return false
+    } else if (!aceptarGenerico && (rfcSinDigito + digitoVerificador === 'XEXX010101000')) {
+      return false
+    }
+    return true
+  }
+
+  isValidCURP(curp: string) {
+    if (!curp) { return false }
+  
+    const re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/
+    const valid = curp.match(re)
+  
+    // Coincide con el formato general?
+    if (!valid) { return false }
+  
+    // Validar que coincida el dígito verificador
+    if (`${valid[2]}` !== `${this.validateVerifiedDigitCURP(valid[1])}`) { return false }
+    return true // Validado
+  }
+  
+  private validateVerifiedDigitCURP (curp17: string) {
+    const diccionario = '0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+    let lngSuma = 0.0
+    let lngDigito = 0.0
+    for (let i = 0; i < 17; i++) { lngSuma = lngSuma + diccionario.indexOf(curp17.charAt(i)) * (18 - i) }
+    lngDigito = 10 - lngSuma % 10
+    if (lngDigito === 10) { return 0 }
+    return lngDigito
   }
 }
