5a9dd6911e3561cfe94ab405cd44bd965673a8e0
wilvardo <wilvardo@gmail.com>
2024-08-17 14:35:25 -0600

    feat(general): starting matrix page

---
+++ pages/documents-expiration-matrix/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Toast />
+    <NuxtLayout name="backoffice">
+      
+    </NuxtLayout>
+  </div>
+</template>
+<script>
+  import Script from './script.ts'
+  export default Script
+</script>
+
+<style lang="scss" scoped>
+  @import './style';
+</style>
