e3245c87a494c2aab4b2970d4e9986367f1e565d
Alberto Escobar <128758041+FernandoHernandezCruz@users.noreply.github.com>
2024-09-03 18:56:10 -0600

    feat(SAEPB0110M001019): Carga de galeria por aeronave

--- resources/scripts/services/GalleryService.ts
+++ resources/scripts/services/GalleryService.ts
@@ -1,27 +1,59 @@
 // GalleryService.ts
-import axios from 'axios';
+import axios from "axios";
 
 class GalleryService {
-  private baseUrl: string;
   protected API_PATH: string;
 
   constructor() {
-    this.baseUrl = 'http://127.0.0.1:3333/api/galleries';
-    const CONFIG = useRuntimeConfig()
+    const CONFIG = useRuntimeConfig();
     this.API_PATH = CONFIG.public.BASE_API_PATH;
-
   }
 
-  public async fetchGallery(galeryIdTable: number, galeryNameTable: string = 'aircrafts', page: number = 1, limit: number = 10) {
+  public async fetchGallery(
+    galeryIdTable: number,
+    galeryNameTable: string = "aircrafts",
+    page: number = 1,
+    limit: number = 10
+  ) {
     try {
-      const response = await axios.get(this.API_PATH + '/galleries', {
+      const response = await axios.get(this.API_PATH + "/galleries", {
         params: {
           page,
           limit,
           galeryIdTable,
-          galeryNameTable
-        }
+          galeryNameTable,
+        },
       });
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async fetchCategories() {
+    try {
+      const response = await axios.get(this.API_PATH + "/galleries");
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteImage(imageId: number) {
+    return axios.delete(this.API_PATH + `/galleries/${imageId}`);
+  }
+
+  public async uploadImage(formData: FormData) {
+    try {
+      const response = await axios.post(
+        this.API_PATH + "/galleries",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
       return response;
     } catch (error) {
       throw error;
