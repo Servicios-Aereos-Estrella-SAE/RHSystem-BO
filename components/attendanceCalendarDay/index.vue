00b33a91b02d36a0e9150014e9c14fc2323892fe
wilvardo <wilvardo@gmail.com>
2024-11-29 08:03:20 -0600

    feat(SAEPB0102 Reporte de asistencia): :construction: SAEPB0102M001081 Reporte de asistencia para turno doble 12x48
    Turno double 12x48 de noche

--- components/attendanceCalendarDay/index.vue
+++ components/attendanceCalendarDay/index.vue
@@ -100,6 +100,9 @@
             </div>
             <div class="time eat-time">
               {{ chekEatInTime || ((checkAssist.assist.checkInStatus === 'fault') ? 'Fault' : '---') }}
+              <span v-if="(chekOutTime && checkAssist.assist.isCheckInEatNextDay)">
+                <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M2.75 4.504a.75.75 0 0 1 .743.648l.007.102v13.499a.75.75 0 0 1-1.493.101L2 18.753v-13.5a.75.75 0 0 1 .75-.75Zm12.46 1.883.083-.094a1 1 0 0 1 1.32-.083l.094.083 4.997 4.998a1 1 0 0 1 .083 1.32l-.083.093-4.996 5.004a1 1 0 0 1-1.499-1.32l.083-.094L18.581 13H6a1 1 0 0 1-.993-.883L5 12a1 1 0 0 1 .883-.993L6 11h12.584l-3.291-3.293a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z" fill="#88a4bf" class="fill-212121"></path></svg>
+              </span>
             </div>
           </div>
           <div class="check">
@@ -108,6 +111,9 @@
             </div>
             <div class="time eat-time">
               {{ chekEatOutTime || ((checkAssist.assist.checkInStatus === 'fault') ? 'Fault' : '---') }}
+              <span v-if="(chekOutTime && checkAssist.assist.isCheckOutEatNextDay)">
+                <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M2.75 4.504a.75.75 0 0 1 .743.648l.007.102v13.499a.75.75 0 0 1-1.493.101L2 18.753v-13.5a.75.75 0 0 1 .75-.75Zm12.46 1.883.083-.094a1 1 0 0 1 1.32-.083l.094.083 4.997 4.998a1 1 0 0 1 .083 1.32l-.083.093-4.996 5.004a1 1 0 0 1-1.499-1.32l.083-.094L18.581 13H6a1 1 0 0 1-.993-.883L5 12a1 1 0 0 1 .883-.993L6 11h12.584l-3.291-3.293a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z" fill="#88a4bf" class="fill-212121"></path></svg>
+              </span>
             </div>
           </div>
           <div class="check">
@@ -116,7 +122,9 @@
             </div>
             <div class="time" :class="checkAssist.assist.checkOutStatus">
               {{ chekOutTime || (checkAssist.assist.checkOutStatus === 'working' ? 'Working' : '---')}}
-              {{ (chekOutTime && checkAssist.assist.isCheckOutNextDay) ? '->' : '' }}
+              <span v-if="(chekOutTime && checkAssist.assist.isCheckOutNextDay)">
+                <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M2.75 4.504a.75.75 0 0 1 .743.648l.007.102v13.499a.75.75 0 0 1-1.493.101L2 18.753v-13.5a.75.75 0 0 1 .75-.75Zm12.46 1.883.083-.094a1 1 0 0 1 1.32-.083l.094.083 4.997 4.998a1 1 0 0 1 .083 1.32l-.083.093-4.996 5.004a1 1 0 0 1-1.499-1.32l.083-.094L18.581 13H6a1 1 0 0 1-.993-.883L5 12a1 1 0 0 1 .883-.993L6 11h12.584l-3.291-3.293a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z" fill="#88a4bf" class="fill-212121"></path></svg>
+              </span>
             </div>
           </div>
           <div class="check info check-notes" :class="{ inactive: !checkAssist.assist.hasExceptions }" @click="displayExceptionComments(checkAssist)">
--- resources/scripts/interfaces/AssistDayInterface.ts
+++ resources/scripts/interfaces/AssistDayInterface.ts
@@ -26,6 +26,8 @@ interface AssistDayInterface {
     exceptions: ShiftExceptionInterface[]
     assitFlatList: AssistInterface[]
     isCheckOutNextDay?: boolean
+    isCheckOutEatNextDay?: boolean
+    isCheckInEatNextDay?: boolean
   }
 }
 
