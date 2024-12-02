009b919fb43b2795d6db47c611943f04b71e5acc
Alberto Escobar <128758041+FernandoHernandezCruz@users.noreply.github.com>
2024-12-02 17:32:59 -0600

    refactor(SAEPB0117M001090):  Control de solicitudes leidas y estatus

--- components/notificationCard/index.vue
+++ components/notificationCard/index.vue
@@ -3,18 +3,18 @@
         <div class="notification-card" v-for="request in filteredNotifications" :key="request.exceptionRequestId"
             @click="redirectToModule(request.exceptionRequestId)">
             <div class="notification-title">
-                <h3>Shift Exception Request</h3>
+                <h3 class="h-text">Shift Exception Request</h3>
             </div>
             <div class="notification-header">
-                <h3>{{ request.type }}</h3>
+                <h3 class="h-text">{{ request.type }}</h3>
             </div>
             <div class="notification-body">
-                <p><strong class="s-title">Department:</strong> {{ request.department }}</p>
-                <p><strong class="s-title">Position:</strong> {{ request.position }}</p>
-                <p><strong class="s-title">Employee Name:</strong> {{ request.employeeName }} </p>
-                <p><strong class="s-title">Date Requested:</strong> {{ formatDate(request.dateRequested) }}</p>
-                <p><strong class="s-title">Description:</strong> {{ request.description }}</p>
-                <p><strong class="s-title">Status:</strong> <span :class="statusClass(request.status)">{{ request.status }}</span></p>
+                <p class="p-text"><strong class="s-title">Department:</strong> {{ request.department }}</p>
+                <p class="p-text"><strong class="s-title">Position:</strong> {{ request.position }}</p>
+                <p class="p-text"><strong class="s-title">Employee Name:</strong> {{ request.employeeName }} </p>
+                <p class="p-text"><strong class="s-title">Date Requested:</strong> {{ formatDate(request.dateRequested) }}</p>
+                <p class="p-text"><strong class="s-title">Description:</strong> {{ request.description }}</p>
+                <p class="p-text"><strong class="s-title">Status:</strong> <span :class="statusClass(request.status)">{{ request.status }}</span></p>
             </div>
         </div>
     </div>
@@ -46,9 +46,10 @@
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
     }
 }
-
+.h-text{
+    font-size: 1rem;
+}
 .s-title{
-    font-size: 0.8 rem;
     font-weight: 500;
 }
 .notification-header {
@@ -57,11 +58,13 @@
 }
 .notification-title{
     border-bottom: 1px dashed #ccc; 
-    padding-bottom: 0.3rem; 
-    margin-bottom: 1rem; 
+    padding-bottom: 0.1rem; 
+    margin-bottom: 0.8rem; 
 }
-.p{
+.p-text{
     font-weight: normal;
+    font-size: 0.8rem;
+
 }
 .notification-body p {
     margin: 0.5rem 0;
