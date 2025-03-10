3e7eea0a7c78c2f9d2bcf9d3b6804740c03468fb
wilvardo <wilvardo@gmail.com>
2025-03-10 14:57:01 -0600

    feat(SAEPB0107 Usuarios): 25SAEPB0107M004029 UI Detailing CRUD usuarios
    UI Tarjeta de usuario

--- components/userInfoCard/index.vue
+++ components/userInfoCard/index.vue
@@ -1,53 +1,51 @@
 <template>
   <div class="box user-info-card">
-    <div class="avatar">
-      <img v-if="getPhoto" :src="getPhoto" alt="Employee Photo" class="employee-photo" />
-      <svg v-else viewBox="0 0 500 500" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
-        <path clip-rule="evenodd"
-          d="M415.762 346.214c-19.078-24.896-42.156-41.063-75.223-50.236l-30.983 108.475c0 9.992-8.181 18.172-18.172 18.172-9.988 0-18.169-8.18-18.169-18.172v-86.311c0-12.536-10.178-22.715-22.715-22.715-12.536 0-22.713 10.179-22.713 22.715v86.311c0 9.992-8.181 18.172-18.17 18.172-9.992 0-18.173-8.18-18.173-18.172l-30.983-108.475c-33.068 9.262-56.145 25.34-75.221 50.236-7.542 9.812-11.64 29.527-11.908 40.07.09 2.725 0 5.906 0 9.082v36.345c0 20.078 16.264 36.34 36.343 36.34h281.648c20.078 0 36.345-16.262 36.345-36.34v-36.345c0-3.176-.089-6.357 0-9.082-.275-10.543-4.368-30.259-11.906-40.07zm-260.66-218.141c0 53.059 33.078 131.013 95.398 131.013 61.237 0 95.396-77.954 95.396-131.013 0-53.057-42.702-96.124-95.396-96.124s-95.398 43.067-95.398 96.124z"
-          fill="#87a4bf" fill-rule="evenodd" class="fill-010101"></path>
-      </svg>
+    <div class="user-head">
+      <div class="avatar">
+        <img v-if="getPhoto" :src="getPhoto" alt="Employee Photo" class="employee-photo" />
+        <div v-else class="username-initial">
+          {{ employeeInitial }}
+        </div>
+        <div class="icon">
+          <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13.94 5 19 10.06 9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L13.938 5Zm7.09-2.03a3.578 3.578 0 0 1 0 5.06l-.97.97L15 3.94l.97-.97a3.578 3.578 0 0 1 5.06 0Z" fill="#88a4bf" class="fill-212121"></path></svg>
+        </div>
+      </div>
+      <div class="user-employee-data">
+        <div class="name capitalize">
+          {{ employeeName }}
+        </div>
+        <div  class="email">
+          {{ `${user.userEmail}`.toLocaleLowerCase() }}
+        </div>
+        <div class="rol capitalize">
+          {{ `${user.role.roleName || ''}`.toLocaleLowerCase() }}
+        </div>
+        <div class="status capitalize">
+          <div class="dot" :class="{ active: !!user.userActive }"></div>
+          {{ user.userActive ? 'Active' : 'Inactive' }} User Access
+        </div>
+      </div>
     </div>
-    <div v-if="user.person" class="name capitalize">
-      {{ `${user.person.personFirstname || ''}`.toLocaleLowerCase() }} {{ `${user.person.personLastname ||
-        ''}`.toLocaleLowerCase() }} {{ `${user.person.personSecondLastname || ''}`.toLocaleLowerCase() }}
+
+    <div class="info">
+      <div class="employee-code">
+        {{ `Employee ID ${getEmployeeNumber  || 'Not specified'}` }}
+      </div>
+      <div class="department capitalize">
+        {{ getDepartment || 'Department not specified' }}
+      </div>
+      <div class="position capitalize">
+        {{ getPosition || 'Position not specified' }}
+      </div>
     </div>
-    <div v-else class="name">
-      ---
-    </div>
-    <div  class="info">
-      {{ `${user.userEmail}`.toLocaleLowerCase() }}
-    </div>
-    <div class="info capitalize">
-      {{ `${user.role.roleName || ''}`.toLocaleLowerCase() }}
-    </div>
-    <div class="info capitalize">
-      {{ user.userActive ? 'Active' : 'Inactive' }}
-    </div>
-    <div class="line">
-     
-    </div>
-    <div v-if="getDepartment" class="info capitalize">
-      {{ getDepartment }}
-    </div>
-    <div v-else class="info">
-      ---
-    </div>
-    <div v-if="getPosition" class="position capitalize">
-      {{ getPosition }}
-    </div>
-    <div v-else class="info">
-      ---
-    </div>
-    <div v-if="getEmployeeNumber" class="number">
-      {{ `No. Emp: ${getEmployeeNumber}` }}
-    </div>
-    <div v-else class="info">
-      ---
-    </div>
-    <div class="box-tools-footer">
-      <Button v-if="canUpdate" icon="pi pi-pencil" class="box-btn" @click="handlerClickOnEdit()"/>
-      <Button v-if="canDelete" icon="pi pi-trash" class="box-btn btn-red" @click="handlerClickOnDelete()"/>
+
+    <div class="user-tools-footer">
+      <button v-if="canUpdate" class="btn btn-block" @click="handlerClickOnEdit">
+        <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13.94 5 19 10.06 9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L13.938 5Zm7.09-2.03a3.578 3.578 0 0 1 0 5.06l-.97.97L15 3.94l.97-.97a3.578 3.578 0 0 1 5.06 0Z" fill="#88a4bf" class="fill-212121"></path></svg>
+      </button>
+      <button v-if="canDelete" class="btn btn-block" @click="handlerClickOnDelete">
+        <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.5 6a1 1 0 0 1-.883.993L20.5 7h-.845l-1.231 12.52A2.75 2.75 0 0 1 15.687 22H8.313a2.75 2.75 0 0 1-2.737-2.48L4.345 7H3.5a1 1 0 0 1 0-2h5a3.5 3.5 0 1 1 7 0h5a1 1 0 0 1 1 1Zm-7.25 3.25a.75.75 0 0 0-.743.648L13.5 10v7l.007.102a.75.75 0 0 0 1.486 0L15 17v-7l-.007-.102a.75.75 0 0 0-.743-.648Zm-4.5 0a.75.75 0 0 0-.743.648L9 10v7l.007.102a.75.75 0 0 0 1.486 0L10.5 17v-7l-.007-.102a.75.75 0 0 0-.743-.648ZM12 3.5A1.5 1.5 0 0 0 10.5 5h3A1.5 1.5 0 0 0 12 3.5Z" fill="#88a4bf" class="fill-212121"></path></svg>
+      </button>
     </div>
   </div>
 </template>
