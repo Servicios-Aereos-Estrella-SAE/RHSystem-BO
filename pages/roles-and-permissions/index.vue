7ab07a0a781feae81bdb44c072e96ac0375cdbed
wilvardo <wilvardo@gmail.com>
2025-03-13 12:45:18 -0600

    fix(SAEPB0107 Usuarios): 25SAEPB0107M004029 UI Detailing CRUD usuarios
    *** Roles y permisos no carga a los departamentos de los roles

--- pages/roles-and-permissions/index.vue
+++ pages/roles-and-permissions/index.vue
@@ -37,6 +37,7 @@
               Create Role
             </button>
           </div>
+
           <div class="box">
             <h2>
               Modules
@@ -57,6 +58,7 @@
               </div>
             </div>
           </div>
+
           <div class="box">
             <h2>
               Departments
@@ -72,52 +74,6 @@
           </div>
         </div>
       </div>
-
-      <!-- <div class="roles-and-permissions-wrapper">
-          <div>
-            <h2>
-              Roles and permissions
-            </h2>
-            <div class="roles-and-permissions-card-wrapper">
-              <Accordion v-model:activeIndex="roleSelected">
-                <AccordionTab :header="role.roleName" v-for="(role, indexRole) in roleList" :key="`role-${role.roleId}-${indexRole}`">
-                  <DataTable :value="systemModulesList" tableStyle="min-width: 50rem">
-                    <Column field="systemModuleName" header="Module">
-                      <template #body="slotProps">
-                        <strong>{{ slotProps.data.systemModuleName }}</strong>
-                      </template>
-                    </Column>
-                    <Column header="Permissions">
-                      <template #body="slotProps">
-                        <div class="permissions">
-                          <div v-for="(permission, indexP) in slotProps.data.systemPermissions"
-                            :key="`permission-${indexRole}-${indexP}`" class="permission">
-                            <Checkbox v-model="permissions[indexRole]" name="permission" :disabled="!canUpdate"
-                              :value="permission.systemPermissionId" />
-                            <label> {{ permission.systemPermissionName }}</label>
-                          </div>
-                        </div>
-                      </template>
-                    </Column>
-                  </DataTable>
-
-                  <DataTable :value="groupedDepartments" tableStyle="min-width: 50rem">
-                    <Column v-for="(dept, index) in groupedDepartments[0]" :key="index" header="Department">
-                      <template #body="slotProps">
-                        <div v-if="slotProps.data[index]">
-                          <Checkbox v-model="departmentPermissions[indexRole]" name="permission" :disabled="!canUpdate"
-                            :value="slotProps.data[index].departmentId" />
-                          <label>&nbsp; {{ slotProps.data[index].departmentName }}</label>
-                        </div>
-                      </template>
-                    </Column>
-                  </DataTable>
-                </AccordionTab>
-              </Accordion>
-            </div>
-          </div>
-        </div> -->
-
     </NuxtLayout>
   </div>
 </template>
