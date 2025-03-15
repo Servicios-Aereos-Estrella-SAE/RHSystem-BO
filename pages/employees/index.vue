d8f935a06fc5ae88f1a82564ee0ccb0410d47d80
wilvardo <wilvardo@gmail.com>
2025-03-15 15:19:08 -0600

    fix(employees): icons

--- pages/employees/index.vue
+++ pages/employees/index.vue
@@ -171,21 +171,13 @@
               Address
             </Button>
             <Button :class="{ 'btn-active': isActive('records') }" class="btn" @click="onEditRecords">
-              <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M20.715 11c.427 0 .855.084 1.257.25C21.59 6.077 17.27 2 12 2 6.477 2 2 6.477 2 12c0 5.184 3.945 9.447 8.997 9.95a2.117 2.117 0 0 1 .064-.543l.458-1.83c.162-.648.497-1.24.97-1.712l5.902-5.903A3.28 3.28 0 0 1 20.713 11h.002Zm0 1h-.002c-.585 0-1.17.223-1.615.67l-5.902 5.902a2.684 2.684 0 0 0-.707 1.247l-.458 1.831a1.087 1.087 0 0 0 1.319 1.318l1.83-.457a2.684 2.684 0 0 0 1.248-.707l5.902-5.902A2.285 2.285 0 0 0 20.715 12Z"
-                  fill="#ffffff" class="fill-212121"></path>
-              </svg>
+              <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17.002 15.244 17 21.245a.75.75 0 0 1-1.182.613l-3.818-2.687-3.817 2.687a.75.75 0 0 1-1.181-.613l-.002-6A7.966 7.966 0 0 0 12 17a7.966 7.966 0 0 0 5.002-1.756ZM12 2a7 7 0 1 1 0 14 7 7 0 0 1 0-14Z" fill="#88a4bf" class="fill-212121"></path></svg>
               Records
             </Button>
-            <Button :class="{ 'btn-active': isActive('banks') }" class="btn" @click="onEditBanks">
-              <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M20.715 11c.427 0 .855.084 1.257.25C21.59 6.077 17.27 2 12 2 6.477 2 2 6.477 2 12c0 5.184 3.945 9.447 8.997 9.95a2.117 2.117 0 0 1 .064-.543l.458-1.83c.162-.648.497-1.24.97-1.712l5.902-5.903A3.28 3.28 0 0 1 20.713 11h.002Zm0 1h-.002c-.585 0-1.17.223-1.615.67l-5.902 5.902a2.684 2.684 0 0 0-.707 1.247l-.458 1.831a1.087 1.087 0 0 0 1.319 1.318l1.83-.457a2.684 2.684 0 0 0 1.248-.707l5.902-5.902A2.285 2.285 0 0 0 20.715 12Z"
-                  fill="#ffffff" class="fill-212121"></path>
-              </svg>
+            <!-- <Button :class="{ 'btn-active': isActive('banks') }" class="btn" @click="onEditBanks">
+              <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M10.968 2.325a1.75 1.75 0 0 1 2.064 0l7.421 5.416c.977.712.474 2.257-.734 2.26H4.28c-1.208-.003-1.71-1.548-.734-2.26l7.421-5.416ZM13 6.25a1 1 0 1 0-2 0 1 1 0 0 0 2 0ZM11.25 16h-2v-5h2v5ZM14.75 16h-2v-5h2v5ZM18.5 16h-2.25v-5h2.25v5ZM18.75 17H5.25A2.25 2.25 0 0 0 3 19.25v.5c0 .415.336.75.75.75h16.5a.75.75 0 0 0 .75-.75v-.5A2.25 2.25 0 0 0 18.75 17ZM7.75 16H5.5v-5h2.25v5Z" fill="#88a4bf" class="fill-212121"></path></svg>
               Banks
-            </Button>
+            </Button> -->
           </div>
 
           <employeeInfoForm v-if="!drawerEmployeePersonForm && !drawerAddressForm && !drawerRecords && !drawerBanks" :employee="employee" @save="onSave" :click-on-edit="() => { onEditPerson(employee) }" />
