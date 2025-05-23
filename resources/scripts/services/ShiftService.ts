42b1e93d2291954df1135cf487d5bfb31bfaa248
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2025-05-22 18:30:52 -0600

    fix(SAEPB0102 Reporte de asistencia): 25SAEPB0102M004046 Duplicidad en asignación y nombres de turno
    en el modulo de turnos se cambiaron las peticiones de guardar para poder mostrar errores de las peticiones al guardar

--- resources/scripts/services/ShiftService.ts
+++ resources/scripts/services/ShiftService.ts
@@ -2,64 +2,65 @@ import type { ShiftInterface } from "../interfaces/ShiftInterface"
 import type { GeneralHeadersInterface } from "../interfaces/GeneralHeadersInterface"
 
 export default class ShiftService {
-    protected API_PATH: string
-    protected GENERAL_HEADERS: GeneralHeadersInterface
+  protected API_PATH: string
+  protected GENERAL_HEADERS: GeneralHeadersInterface
 
-    constructor () {
-      const CONFIG = useRuntimeConfig()
-      this.API_PATH = CONFIG.public.BASE_API_PATH
-      const { token } = useAuth()
-      this.GENERAL_HEADERS = {
-        Authorization: `${token.value}`
+  constructor() {
+    const CONFIG = useRuntimeConfig()
+    this.API_PATH = CONFIG.public.BASE_API_PATH
+    const { token } = useAuth()
+    this.GENERAL_HEADERS = {
+      Authorization: `${token.value}`
+    }
+  }
+
+  async getFilteredList(searchText: string, page: number = 1, limit: number = 10) {
+    let responseRequest: any = null
+    const headers = { ...this.GENERAL_HEADERS }
+
+    await $fetch(`${this.API_PATH}/shift`, {
+      headers,
+      query: {
+        shiftName: searchText,
+        page,
+        limit
+      },
+      onResponse({ response }) {
+        responseRequest = response
+      },
+      onRequestError({ response }) {
+        responseRequest = response
+      }
+    })
+
+    if (responseRequest?.data) {
+      return {
+        data: responseRequest.data.data,
+        meta: responseRequest.data.meta
       }
     }
-  
-    async getFilteredList(searchText: string, page: number = 1, limit: number = 10) {
-      let responseRequest: any = null
-      const headers = { ...this.GENERAL_HEADERS }
 
-      await $fetch(`${this.API_PATH}/shift`, {
-        headers,
-        query: {
-          shiftName: searchText,
-          page,
-          limit
-        },
-        onResponse({ response }) {
-          responseRequest = response
-        },
-        onRequestError({ response }) {
-          responseRequest = response
-        }
-      })
-  
-      if (responseRequest?.data) {
-        return {
-          data: responseRequest.data.data,
-          meta: responseRequest.data.meta
-        }
-      }
-  
-      return responseRequest
-    }
+    return responseRequest
+  }
 
-    async delete(shift: ShiftInterface) {
-      let responseRequest: any = null;
-      const headers = { ...this.GENERAL_HEADERS }
+  async delete(shift: ShiftInterface) {
+    let responseRequest: any = null;
+    const headers = { ...this.GENERAL_HEADERS }
 
-      await $fetch(`${this.API_PATH}/shift/${shift.shiftId}`, {
-        headers,
-        method: 'DELETE',
-        onResponse ({ response }) { responseRequest = response },
-        onRequestError ({ response }) { responseRequest = response }
-      });
-  
-      return responseRequest;
-    }
+    await $fetch(`${this.API_PATH}/shift/${shift.shiftId}`, {
+      headers,
+      method: 'DELETE',
+      onResponse({ response }) { responseRequest = response },
+      onRequestError({ response }) { responseRequest = response }
+    });
 
-    async create(shift: ShiftInterface) {
-      const headers = { ...this.GENERAL_HEADERS }
-      let responseRequest: any = null
+    return responseRequest;
+  }
+
+  async create(shift: ShiftInterface) {
+    const headers = { ...this.GENERAL_HEADERS }
+    let responseRequest: any = null
+    try {
       await $fetch(`${this.API_PATH}/shift`, {
         headers,
         method: 'POST',
@@ -67,12 +68,15 @@ export default class ShiftService {
         onResponse({ response }) { responseRequest = response },
         onRequestError({ response }) { responseRequest = response }
       })
-      return responseRequest
+    } catch (error) {
     }
-  
-    async update(shift: ShiftInterface) {
-      const headers = { ...this.GENERAL_HEADERS }
-      let responseRequest: any = null
+    return responseRequest
+  }
+
+  async update(shift: ShiftInterface) {
+    const headers = { ...this.GENERAL_HEADERS }
+    let responseRequest: any = null
+    try {
       await $fetch(`${this.API_PATH}/shift/${shift.shiftId}`, {
         headers,
         method: 'PUT',
@@ -80,7 +84,8 @@ export default class ShiftService {
         onResponse({ response }) { responseRequest = response },
         onRequestError({ response }) { responseRequest = response }
       })
-      return responseRequest
+    } catch (error) {
     }
+    return responseRequest
   }
-  
+}
