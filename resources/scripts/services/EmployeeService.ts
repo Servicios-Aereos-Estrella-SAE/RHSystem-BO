5db2db541dbcae3159ff3aa40459f70045b6ef5f
joseguadalupesotobecerra9605 <jzoto@siler-mx.com>
2025-07-24 21:43:49 -0600

    fix(SAEPB0108 Vacaciones): 25SAEPB0108M004054 Reporte de resumen de control
    se agrego un nuevo botón en el modulo de calendario de vacaciones para descargar el resumen de vacaciones por año

--- resources/scripts/services/EmployeeService.ts
+++ resources/scripts/services/EmployeeService.ts
@@ -520,5 +520,36 @@ export default class EmployeeService {
     return responseRequest
   }
 
+  async getVacationsSummaryExcel(searchText: string, departmentId: number | null, positionId: number | null,
+    startDate: string | Date,
+    endDate: string | Date,
+    onlyInactive: boolean
+  ) {
+    let responseRequest: any = null
+    try {
+      const query = {
+        search: searchText,
+        startDate: startDate,
+        endDate: endDate,
+        departmentId: departmentId,
+        positionId: positionId,
+        onlyInactive: onlyInactive
+      }
+      await $fetch(`${this.API_PATH}/employees-vacations/get-vacations-summary-excel`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          ...this.GENERAL_HEADERS,
+        },
+        query,
+        onResponse({ response }) { responseRequest = response },
+        onRequestError({ response }) { responseRequest = response?.json() }
+      })
+    } catch (error) {
+    }
+    return responseRequest
+  }
+
+
 }
 
